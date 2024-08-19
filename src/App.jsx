72d26f572d32dc526b636dfaecdfdec630c68e30
Drew Gragg <98364811+dlhg@users.noreply.github.com>
2024-08-19 10:44:50 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,7 @@ import * as Tone from "tone";
 function App() {
   const [page, setPage] = useState(0);
 
-  return (
-    <>
-      <div className="viewport">
-        <div className="grid-parent">
-          <div className="sequencer">
-            <div className="solo-mute-volume-column"></div>
-          </div>
-
-          <div className="left-menu"> </div>
-          <div className="bottom-menu"></div>
-          <div className="top-menu"> </div>
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
