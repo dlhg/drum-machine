bca9043d23e75e4037b7e70325c4277e73fc74df
Drew Gragg <98364811+dlhg@users.noreply.github.com>
2024-08-20 11:05:16 -0400

    Now all 12 notes can be selected, not just notes from C major

--- src/components/StepSequencer.jsx
+++ src/components/StepSequencer.jsx
@@ -145,7 +145,20 @@ const StepSequencer = () => {
               onChange={(e) => handleNoteChange(rowIndex, e)}
               className="note-dropdown"
             >
-              {["C3", "D3", "E3", "F3", "G3", "A3", "B3"].map((note) => (
+              {[
+                "C3",
+                "C#3",
+                "D3",
+                "D#3",
+                "E3",
+                "F3",
+                "F#3",
+                "G3",
+                "G#3",
+                "A3",
+                "A#3",
+                "B3",
+              ].map((note) => (
                 <option key={note} value={note}>
                   {note}
                 </option>
