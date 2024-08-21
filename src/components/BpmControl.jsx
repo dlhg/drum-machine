60f6ecd5c60d6083e3310a72c259d33d694631d1
Drew Gragg <98364811+dlhg@users.noreply.github.com>
2024-08-21 17:54:12 -0400

    changed BPMControl component to be a number type input instead of range

--- src/components/BpmControl.jsx
+++ src/components/BpmControl.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BpmControl = ({ bpm, minBPM, maxBPM, handleBpmChange }) => (
   <div className="bpm-control">
     <input
-      type="range"
+      type="number"
       min={minBPM}
       max={maxBPM}
       value={bpm}
