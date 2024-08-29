c674ff82de4b017ebd24e8acf0faabe949579466
Drew Gragg <98364811+dlhg@users.noreply.github.com>
2024-08-29 16:57:37 -0400

    chore: Update font-family in App.css and set Tone.Transport swing to 0 in StepSequencer.jsx

--- src/components/StepSequencer.jsx
+++ src/components/StepSequencer.jsx
@@ -169,6 +169,8 @@ const StepSequencer = () => {
 
   useEffect(() => {
     Tone.Transport.bpm.value = bpm;
+    // 0 swing is default, just putting this here in case I want to add swing later
+    Tone.Transport.swing = 0;
 
     const loop = new Tone.Sequence(
       (time, step) => {
