abecd77c466fdfcb8088d37e9531ce9645e49ad7
Drew Gragg <98364811+dlhg@users.noreply.github.com>
2024-09-26 12:38:47 -0400

    added pagination to stepgrid

--- src/components/StepGrid.jsx
+++ src/components/StepGrid.jsx
@@ -10,6 +10,8 @@ const StepGrid = ({
   sampleNames,
 }) => {
   const [clickedStep, setClickedStep] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+  const stepsPerPage = 8;
 
   const handleStepClick = (rowIndex, stepIndex) => {
     toggleStep(rowIndex, stepIndex);
@@ -20,6 +22,17 @@ const StepGrid = ({
       setClickedStep(null);
     }, 300);
   };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+  };
+
+  const startStepIndex = currentPage * stepsPerPage;
+  const endStepIndex = startStepIndex + stepsPerPage;
 
   return (
     <div className="sequencer-grid">
@@ -32,26 +45,39 @@ const StepGrid = ({
             handleNoteChange={handleNoteChange}
           />
           <div className="steps-container">
-            {row.map((step, stepIndex) => (
+            {row.slice(startStepIndex, endStepIndex).map((step, stepIndex) => (
               <div
                 key={stepIndex}
-                onClick={() => handleStepClick(rowIndex, stepIndex)}
+                onClick={() =>
+                  handleStepClick(rowIndex, startStepIndex + stepIndex)
+                }
                 className={`step ${step ? "active" : ""} ${
-                  currentStep === stepIndex ? "playing" : ""
+                  currentStep === startStepIndex + stepIndex ? "playing" : ""
                 } ${
                   clickedStep &&
                   clickedStep.rowIndex === rowIndex &&
-                  clickedStep.stepIndex === stepIndex
+                  clickedStep.stepIndex === startStepIndex + stepIndex
                     ? "click-pulse"
                     : ""
                 }`}
               >
-                {stepIndex + 1}
+                {startStepIndex + stepIndex + 1}
               </div>
             ))}
           </div>
         </div>
       ))}
+      <div className="pagination-controls">
+        <button onClick={handlePrevPage} disabled={currentPage === 0}>
+          &lt; Prev
+        </button>
+        <button
+          onClick={handleNextPage}
+          disabled={endStepIndex >= sequence[0].length}
+        >
+          Next &gt;
+        </button>
+      </div>
     </div>
   );
 };
